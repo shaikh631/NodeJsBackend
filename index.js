10125381c86bd8f0b7f8668965274953cec2c3ee
shaikh631 <as9251145@gmail.com>
2026-08-04 03:50:07 +0530

    Installing Node js and Express js

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+const port = process.env.Port || 4000;
+
+app.get( '/',(req , res) => {
+    res.send("Hello World....")
+})
+app.listen(port , () => {
+    console.log(`Exmaple app listing on Port ${port}`)
+})
