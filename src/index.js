34076bb86bb33ea03826f4754a6fefcec2b36231
shaikh631 <as9251145@gmail.com>
2026-08-17 05:40:43 +0530

    API Response

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import dotenv from 'dotenv'
 import ConnectDB from "./db/index.js";
 
 dotenv.config({path : '/.env'})
-ConnectDB();
+ConnectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`Server is running on Port :${process.env.PORT }`);
+    })
+})
+.catch((error) => {
+    console.log(`MonogoDB Connection Failed !!! ${error}`);
+})
