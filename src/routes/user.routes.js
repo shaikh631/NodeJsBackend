6c16d20091b74c582dc472968ffdd4a1531ca2dd
shaikh631 <as9251145@gmail.com>
2026-09-22 19:38:43 +0530

    adding Router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -26,5 +26,12 @@ router.route('/register').post(
     router.route('/login').post(loginUser)
     router.route('/logout').post(VerifyJWT , logout)
     router.route('/refresh-token').post(refreshAccessToken)
+    router.route('change-password').post(VerifyJWT ,changeCurrentPassword )
+    router.route('/current-user').get(VerifyJWT , getCurrentUser)
+    router.route('/update-account').patch(VerifyJWT , UpdateAccountDetails)
+    router.route('/update-avatar').patch(VerifyJWT , upload.single('avatar') , updateUserAvatar),
+    router.route('/update-coverImage').patch(VerifyJWT , upload.single('coverImage') , updateUserCoverImage),
+    router.route('/c/:username').get(VerifyJWT , getUserChannelProfile),
+    router.route('/watch-history').get(VerifyJWT , getWatchHistory)
 
 export default router;
